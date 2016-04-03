2a533b16b2f904301ba3d368dbd858808b6830f2
Emil Westergren <emilwestergren@gmail.com>
2016-04-03 17:46:28 +0200

    Added creation of vkInstance.

--- VulkanDemo/main.cpp
+++ VulkanDemo/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <vulkan/vulkan.h>
+#define VK_MAKE_VERSION(major, minor, patch) \
+    (((major) << 22) | ((minor) << 12) | (patch))
 
 int main() {
 	printf("HELLO WORLD");
-	for (auto i = 0; i < 100; i++)
-		printf("HEJ");
+	VkResult errorCode;
+	VkApplicationInfo appInfo = {
+		VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		NULL,
+		"VulkanDemo",
+		0,
+		"VulkanDemoEngine",
+		0,
+		VK_MAKE_VERSION(1, 0, 0)
+	};
+	VkInstanceCreateInfo instanceInfo = {
+		VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		NULL,
+		NULL,
+		&appInfo,
+		0,
+		NULL,
+		0,
+		NULL
+	};
+	VkInstance instance;
+	errorCode = vkCreateInstance(&instanceInfo, NULL, &instance);
+	if (errorCode != VK_SUCCESS)
+		printf("Failed to create Vulkan instance");
+	std::cin.get();
+	
 	return 0;
 }
